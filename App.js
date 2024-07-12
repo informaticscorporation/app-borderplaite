82f83f4cca3f50eba3b945268ab70d3f4d92eab9
informaticscorporation <152265894+informaticscorporation@users.noreply.github.com>
2024-07-12 18:50:29 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{ uri: 'https://menumarinadelre-nu.vercel.app/' }}
+        style={{ flex: 1 }}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+});
